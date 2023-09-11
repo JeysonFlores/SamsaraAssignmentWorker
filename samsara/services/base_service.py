3eda7d20f0b4ab617bf6941c8e733434b7b9a02a
JeysonFlores <jeyson.flores@hotmail.com>
2023-09-10 18:20:40 -0700

    Base service for samsara services done

---
+++ samsara/services/base_service.py
@@ -0,0 +1,44 @@
+import os
+import requests
+
+from samsara.services.common import SamsaraServiceError
+
+
+class BaseService:
+    def __init__(self, endpoint: str, auth_token: str, is_legacy: bool = False) -> None:
+        super().__init__()
+        self.domain = os.environ["SAMSARA_API"]
+        self.endpoint = endpoint
+        self.is_legacy = is_legacy
+        self.auth_token = auth_token
+        self.headers = {"Authorization": f"Bearer {auth_token}"}
+
+    def _prepare_request(self, method: str, body: dict = {}, params: dict = {}):
+        """
+        Generic request creation. Takes in consideration if the service uses a legacy endpoint.
+        """
+        url = f"{self.domain}{'/v1' if self.is_legacy else ''}{self.endpoint}"
+
+        return requests.request(
+            method, url, params=params, json=body, headers=self.headers
+        )
+
+    def _validate_response(self, response: requests.Response):
+        """
+        Validates response to generic filters and parses response into JSON format
+        """
+        if response.status_code not in [200, 201]:
+            raise SamsaraServiceError(f"Error in Samsara request: {response.text}:")
+
+        if "application/json" not in response.headers.get("Content-Type").lower():
+            raise SamsaraServiceError(
+                f"Samsara API responded with invalid format: {response.text}"
+            )
+
+        return response.json()
+
+    def make_request(self, method: str, body: dict = {}, params: dict = {}):
+        """
+        Exposed method for chaining all generic private methods of the service.
+        """
+        return self._validate_response(self._prepare_request(method, body, params))
