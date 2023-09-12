ca682480aab0e0706141523540b31707f4cb734b
JeysonFlores <jeyson.flores@hotmail.com>
2023-09-11 22:23:48 -0700

    Added common functions

---
+++ worker/common/common_functions.py
@@ -0,0 +1,23 @@
+import json
+
+from worker.common.util_functions import create_connection_to_redis
+
+
+redis_connection = create_connection_to_redis()
+
+
+def get_value_from_redis(redis_key, to_dict=True):
+    try:
+        value = redis_connection.get(redis_key)
+        if to_dict and value:
+            value = json.loads(value)
+    except Exception as _:
+        value = None
+
+    return value
+
+
+def set_value_into_redis(redis_key, payload, ttl=None):
+    redis_connection.set(redis_key, json.dumps(payload))
+    if ttl is not None and type(ttl) is int:
+        redis_connection.expire(redis_key, ttl)
