f576e226777076aeada80925c0e37a86f4c01cc5
JeysonFlores <jeyson.flores@hotmail.com>
2023-09-11 22:23:34 -0700

    Added util functions

---
+++ worker/common/util_functions.py
@@ -0,0 +1,24 @@
+import os
+import redis
+import logging
+
+
+logger = logging.getLogger("worker")
+logger.setLevel(logging.INFO)
+
+
+def create_connection_to_redis():
+    host = os.environ.get("REDIS_HOST")
+    port = int(os.environ.get("REDIS_PORT"))
+    username = os.environ.get("REDIS_USERNAME")
+    password = os.environ.get("REDIS_PASSWORD")
+    return redis.Redis(
+        host=host,
+        port=port,
+        username=username,
+        password=password,
+        decode_responses=True,
+    )
+
+def get_logger():
+    return logger
